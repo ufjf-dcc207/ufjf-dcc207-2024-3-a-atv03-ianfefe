849dd8836d3b839b54e92988aa10f987a5aab8f7
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-02 20:49:44 -0300

    adiciona mais animais

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 
 function App() {
   return <div className='app'>
-    <Animal icone="🦁" nome="Leão" peso={190.37} extincao={true}/>
+    <Animal icone="🦁" nome="Leão" peso={0} extincao/>
+    <Animal icone="🦒" nome="Girafa" peso={1200} extincao/>
+    <Animal icone="🦜" nome="Papagaio" peso={0.12} extincao={false}/>
     
   </div>;
 }
