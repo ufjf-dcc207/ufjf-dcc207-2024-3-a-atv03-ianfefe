86550773c20fd4c2a052ca0fdf4df7e9b2225ba5
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-02 20:29:33 -0300

    adiciona animal e estilo

---
+++ src/Animal.tsx
@@ -0,0 +1,18 @@
+import "./Animal.css";
+
+interface AnimalProps {
+    icone: string;
+    nome: string;
+    peso: number;
+    extincao: boolean
+}
+
+export default function Animal({icone, nome, peso, extincao}: AnimalProps) {
+  return (
+    <div className="animal">
+      <div className="icone">{icone}</div>
+      <div className="nome">{nome}</div>
+      <div className="peso">{peso.toLocaleString()}kg</div>
+      <div className="extincao">{extincao ? "true" : "false"}</div>
+  </div>)
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import Animal from "./Animal";
+import "./App.css";
 
 function App() {
-  return (
-  <>
-    <div className='app'>Olá mundo!</div>
-  </> 
-  )
+  return <div className='app'>
+    <Animal icone="🦁" nome="Leão" peso={190} extincao={true}/>
+    
+  </div>;
 }
 
 export default App
