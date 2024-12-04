250d634e255116543ff49ea3fde78a9edd2b046d
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-04 15:52:10 -0300

    adiciona base da static page

--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,21 +1,25 @@
+import { ReactNode } from "react";
 import "./Exibicao.css"
 
 interface ExibicaoProps {
     inicio: Date,
     fim: Date,
     local: string;
+    children?: ReactNode;
 }
 
 export default function Exibicao ({
     inicio,
     fim,
-    local: cercado,
+    local,
+    children,
 }: ExibicaoProps) {
     return (
         <div className="exibicao">
             <div className="inicio">Abertura: {inicio.toLocaleString()}</div>
             <div className="fim">Encerramento: {fim.toLocaleString()}</div>
-            <div className="local">Local: {cercado.toLocaleString()}</div>
+            <div className="cercado">Local: {local.toLocaleString()}</div>
+            <div className="animais">{children}</div>
         </div>
     );
 }
