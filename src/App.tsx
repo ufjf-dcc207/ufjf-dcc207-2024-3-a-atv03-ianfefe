b819c0e20aa722d1bec59e14e784438917203ee5
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-05 22:15:23 -0300

    adiciona animal como componente filho de exibicao

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Exibicao from "./Exibicao";
 
 function App() {
   return <div className='app'>
-    <Exibicao inicio={new Date("2024-12-02T08:00-03:00")} fim={new Date("2024-12-02T12:00-03:00")} local="A1"/>
-    <Animal icone="🦁" nome="Leão" peso={0} extincao/>
-    <Animal icone="🦒" nome="Girafa" peso={1200} extincao/>
-    <Exibicao inicio={new Date("2024-12-02T14:00-03:00")} fim={new Date("2024-12-02T16:00-03:00")} local="A1"/>
-    <Animal icone="🦜" nome="Papagaio" peso={0.12} extincao={false}/>
-    
+    <Exibicao inicio={new Date("2024-12-02T08:00-03:00")} fim={new Date("2024-12-02T12:00-03:00")} local="A1">
+      <Animal icone="🦁" nome="Leão" peso={0} extincao/>
+      <Animal icone="🦒" nome="Girafa" peso={1200} extincao/>
+    </Exibicao>
+    <Exibicao inicio={new Date("2024-12-02T14:00-03:00")} fim={new Date("2024-12-02T16:00-03:00")} local="A1">
+      <Animal icone="🦜" nome="Papagaio" peso={0.12} extincao={false}/>
+    </Exibicao>
+
   </div>;
 }
 
