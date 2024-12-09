4faf41dcb849fb1325fff7ae68d00b3dadfc10de
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-09 20:12:48 -0300

    filtra os animais por exibicao

--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,33 @@ const ANIMAIS: AnimaisTuplaType[] = [
 ];
 
 function App() {
-  const exA1: ReactNode[] = [];
-  const exB2: ReactNode[] = [];
-  for (let i = 0; i < ANIMAIS.length; i++) {
-    const ex = ANIMAIS[i][3] != false ? exA1 : exB2
-    ex.push(
-      <Animal
-        key={ANIMAIS[i][1]}
-        icone={ANIMAIS[i][0]}
-        nome={ANIMAIS[i][1]}
-        peso={ANIMAIS[i][2]}
-        extincao={ANIMAIS[i][3]}
-      />
-    );
-  }
+  const exA1: AnimaisTuplaType[] = ANIMAIS.filter((animal => animal[3] == true));
+
+  const exB2: AnimaisTuplaType[] = ANIMAIS.filter((animal => animal[3] == false));
 
   return (
     <div className='app'>
       <Exibicao inicio={new Date("2024-12-02T08:00-03:00")} fim={new Date("2024-12-02T12:00-03:00")} local="A1">
-        <div>{exA1}</div>
+        {exA1.map((animal) => (
+          <Animal
+            key={animal[1]}
+            icone={animal[0]}
+            nome={animal[1]}
+            peso={animal[2]}
+            extincao={animal[3]}
+          />
+        ))}
       </Exibicao>
       <Exibicao inicio={new Date("2024-12-02T14:00-03:00")} fim={new Date("2024-12-02T16:00-03:00")} local="B2">
-        <div>{exB2}</div>
+        {exB2.map((animal) => (
+            <Animal
+              key={animal[1]}
+              icone={animal[0]}
+              nome={animal[1]}
+              peso={animal[2]}
+              extincao={animal[3]}
+            />
+          ))}
       </Exibicao>
 
     </div>
